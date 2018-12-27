e3c081816fb3bca8dd7edf799a8e0037fb2e9fc2
aelkner <alan.elkner@tufin.com>
2018-12-27 14:14:38 -0500

    Set to version 1.0.0

--- traversify/metadata.py
+++ traversify/metadata.py
@@ -1,7 +1,6 @@
-__version__ = "0.0.2"
+__version__ = "1.0.0"
 __author__ = "Alan Elkner"
 __copyright__ = "Copyright 2018, Alan Elkner"
 __maintainer__ = "Alan Elkner"
 __email__ = "aelkner@gmail.com"
 __license__ = "GNU GENERAL PUBLIC LICENSE version 3"
-
